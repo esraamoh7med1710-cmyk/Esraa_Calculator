2f2439e499894e540a999f74fa52841fc53dd87c
Esraa mohamed <esraamoh7med1710@gmail.com>
2026-02-24 05:53:59 -0800

    Add files via upload

---
+++ Esraa_Calculator.py.py
@@ -0,0 +1,23 @@
+print("Hello!This is Engineer Esraa 's Calculator")
+while True:
+ num1 =float(input("Enter first number:"))
+ num2 =float(input("Enter Second number:"))
+            
+ op=input("Enter operation (+,-,*,/):")
+ if op =="+":
+   print("Result is:",num1+num2)
+ elif op=="-":
+    print("Result is:",num1-num2)
+ elif op=="*":
+    print("Result is:",num1*num2)
+ elif op=="/":
+    print("Result is:",num1/num2)
+ if num2!=0:
+    print("Result is:",num1/num2)
+ else:
+    print("Error!Divission by zero")
+    
+    
+    
+    
+  
